1396112b86fc364137713094dbf3d1e488868597
zion830 <zion830@naver.com>
2019-02-19 01:08:24 +0900

    새 탭 시계 구현

--- js/clock.js
+++ js/clock.js
@@ -1,23 +1,32 @@
-const clockContainer = document.querySelector(".clock"),
-    clockTitle = clockContainer.querySelector("h1");
+function getDate() {
+    const date = new Date();
+    const weeks = ['일', '월', '화', '수', '목', '금', '토'];
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    return `${date.getFullYear()}.${
+        month < 10 ? `0${month}` : month}.${
+        day < 10 ? `0${day}` : day} ${
+        weeks[date.getDay()]}요일`
+}
 
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
     let hours = date.getHours();
     const seconds = date.getSeconds();
-    let scope = 'AM';
+    let period = '<span class="datetime">오전</span>';
 
-    if (hours > 12) {
-        hours = hours - 12;
-        scope = 'PM';
+    if (hours >= 12) {
+        period = '<span class="datetime">오후</span>';
     }
 
-    clockTitle.innerText =
-        `${
-            hours < 10 ? `0${hours}` : hours}:${
+    $(".clock>h1").html(
+        `<span class="date"> ${getDate()} </span> <br>
+        ${period} ${
+            hours < 10 ? ((hours === 0) ? '12' : `0${hours}`) : hours}:${
             minutes < 10 ? `0${minutes}` : minutes}:${
-            seconds < 10 ? `0${seconds}` : seconds} ${scope}`
+            seconds < 10 ? `0${seconds}` : seconds}`);
 }
 
 function init() {
