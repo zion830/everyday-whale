bcf2d8ca0e36b6b0240e3df94a53ea8d7ade364c
zion830 <zion830@honglab.org>
2019-09-24 09:35:11 +0900

    할일 탭에서 일정하게 10개씩 보여주지 않는 페이징 오류 수정

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    var todoList;
+    let todoList;
 
     try {
         todoList = JSON.parse(localStorage["todoList"]);
@@ -7,20 +7,20 @@ window.onload = function () {
         todoList = {}
     }
 
-    var str = '<ul class="todo-list">';
-    var idx = 0;
-    var pageNum = Number(location.href.substr(location.href.lastIndexOf('=') + 1));
+    let str = '<ul class="todo-list">';
+    let idx = 0;
+    const pageNum = Number(location.href.substr(location.href.lastIndexOf('=') + 1));
 
     if (todoList.length > 0) {
         todoList.reverse();
 
-        var start = pageNum * 10;
-        var end = start + 10;
-        var startIdx = 0, endIdx = 0;
+        const start = pageNum * 10;
+        const end = start + 10;
+        let startIdx = 0, endIdx = 0;
 
         todoList.forEach(value => {
             if (value.finishDate != null || value.isChecked) {
-                if (startIdx++ >= start && endIdx++ < end) { // 한 페이지에 10개 씩
+                if (startIdx >= start && endIdx < end) { // 한 페이지에 10개 씩
                     str += '<li class="finish-item">' + value.todo
                         + '<br><span class="period">진행기간 | ' +
                         value.addDate + " ~ ";
@@ -31,7 +31,8 @@ window.onload = function () {
                         str += getDateStr() + '*' + '</span></li>';
                 }
                 idx++;
-                console.log(value.todo);
+                startIdx++;
+                endIdx++;
             }
         });
 
@@ -41,6 +42,7 @@ window.onload = function () {
     } else {
         $('#no-item-msg').text("완료된 할 일이 이곳에 표시됩니다!");
     }
+
     str += '</ul>';
     $('#finish-list').html(str);
 
@@ -59,8 +61,6 @@ window.onload = function () {
 };
 
 function getDateStr() {
-    var date = new Date();
-    var dateStr = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-
-    return dateStr;
+    const date = new Date();
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 }
