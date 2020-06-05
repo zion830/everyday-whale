09ab807a03b3c94b5a0f46d478b97ba912feab8c
Sunny.K <sunnyk5780@gmail.com>
2020-06-05 20:52:20 +0900

    setting.js에서 jquery 제거

--- js/setting.js
+++ js/setting.js
@@ -1,20 +1,17 @@
+// 초기화하기
 function clearData() {
-    var clearConfirm = confirm("정말 초기화 하시겠습니까?\n모든 데이터가 영구적으로 삭제됩니다.");
+    const clearConfirm = confirm("정말 초기화 하시겠습니까?\n모든 데이터가 영구적으로 삭제됩니다.")
 
     if (clearConfirm) {
-        localStorage.removeItem("name");
-        localStorage.removeItem("level");
-        localStorage.removeItem("sumCount");
-        localStorage.removeItem("todoList");
-        localStorage.removeItem("tabStatus");
+        localStorage.removeItem("name")
+        localStorage.removeItem("level")
+        localStorage.removeItem("sumCount")
+        localStorage.removeItem("todoList")
+        localStorage.removeItem("tabStatus")
 
-        history.replaceState({}, "main", "start.html");
-        location.reload();
+        history.replaceState({}, "main", "start.html")
+        location.reload()
     }
 }
 
-$(function () {
-    $('.btn-clear').click(function () {
-        clearData();
-    });
-});
+document.querySelector('.btn-clear').addEventListener('click', clearData)
